93228ebdf112a4017e44f70f855a013a6af89896
Shawn <shawntayws@gmail.com>
2023-11-22 14:12:23 +0800

    simple working chatgpt

--- main.py
+++ main.py
@@ -1,4 +1,36 @@
 import os
 import typer
-import openai
-from dotenv import laod_dotenv
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+    api_key = os.getenv("OPENAI_KEY")
+)
+
+app = typer.Typer()
+
+@app.command()
+def interactive_chat():
+    """Interactive CLI tool to chat with ChatGPT"""
+    typer.echo(
+        "Starting interactive chat with ChatGPT. Type 'exit' to end the session"
+    )
+
+    while True:
+        prompt = input("User: ")
+        if prompt == "exit":
+            typer.echo("ChatGPT: Goodbye!")
+            break
+        
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo", 
+            messages=[{"role":"user", "content":prompt}]
+        )
+
+        typer.echo(f'ChatGPT: {response.choices[0].message.content}')
+
+
+if __name__ == "__main__":
+    app()
