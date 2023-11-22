a060aaef25bea302ffee12bf9a98c715221816ad
Shawn <shawntayws@gmail.com>
2023-11-22 14:46:25 +0800

    messages now have memory, but memory data might need cleanup

--- main.py
+++ main.py
@@ -18,18 +18,23 @@ def interactive_chat():
         "Starting interactive chat with ChatGPT. Type 'exit' to end the session"
     )
 
+    messages = []
+
     while True:
         prompt = input("User: ")
+        messages.append({"role":"user", "content":prompt})
         if prompt == "exit":
             typer.echo("ChatGPT: Goodbye!")
             break
         
         response = client.chat.completions.create(
             model="gpt-3.5-turbo", 
-            messages=[{"role":"user", "content":prompt}]
+            messages=messages
         )
 
         typer.echo(f'ChatGPT: {response.choices[0].message.content}')
+        messages.append(response.choices[0].message)
+
 
 
 if __name__ == "__main__":
